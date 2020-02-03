c18d07ae5cc2277081881814de1df4eac48173b4
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-03 11:18:02 +0300

    Added code to display all pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,28 @@ class PagesController extends Controller
         $title = "Medref | Home";
         return view('welcome')->with(compact('title'));
     }
+    public function about(){
+        $title = "Medref | About Us";
+        return view('about')->with(compact('title'));
+    }
+    public function service(){
+        $title = "Medref | What We Do";
+        return view('service')->with(compact('title'));
+    }
+     public function blog(){
+        $title = "Medref | Our Blogs ";
+        return view('single_blog')->with(compact('title'));
+    }
+     public function showContactForm(){
+        $title = "Medref | Contact Us";
+        return view('contact')->with(compact('title'));
+    }
+    public function createPage(Request $request) {
+        if($request ){
+            
+        }
+        $title = "Medref | Page Management";
+        return view('pages.create')->with(compact('title'));
+        
+    }
 }
